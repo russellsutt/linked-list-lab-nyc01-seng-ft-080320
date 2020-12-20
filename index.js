cea58af2476fb3355c66beff8ccad7234e67e1e2
Russell Sutter <russell.sutter@gmail.com>
2020-12-20 21:36:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ function insertNodeAt(index, newNodeAddress, linkedlist, collection) {
   let previousNode = nodeAt(index - 1, linkedlist, collection)
   let nextNode = nodeAt(index, linkedlist, collection)
   
-  let previousNodeIndex = indexAt(previousNode, collection, linkedlist)
+
+  
   let previousNodeAddress = addressAt(previousNode, linkedlist, collection)
-  let nextNodeInde = indexAt(nextNode, collection, linkedlist)
   let nextNodeAddress = addressAt(nextNodeAddress, linkedlist, collection)
   
   previousNode.next = newNodeAddress
